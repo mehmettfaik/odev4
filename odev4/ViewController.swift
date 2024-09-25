a55f96492725141c1ec4a45858da7d5a3038c83f
Mehmet Faik <mehmettfaik@gmail.com>
2024-09-25 19:05:26 +0300

    add

---
+++ odev4/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  odev4
+//
+//  Created by Mehmet Faik Ayhan on 24.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
---
+++ odev4/ViewControllerX.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerX.swift
+//  odev4
+//
+//  Created by Mehmet Faik Ayhan on 24.09.2024.
+//
+
+import UIKit
+
+class ViewControllerX: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
